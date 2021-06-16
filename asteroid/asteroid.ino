d01155472c0b3011d083661a82ac5325d5857674
DUCROUX <jacques.ducroux@gmail.com>
2021-06-16 17:47:35 +0200

    The ship will not leave the screen

--- asteroid/asteroid.ino
+++ asteroid/asteroid.ino
@@ -20,24 +20,41 @@ void loop()
     gb.display.drawRect(ship_pos_x, ship_pos_y, 5, 5);
 
     //SHIP MOVEMENTS
-    if (gb.buttons.pressed(BUTTON_UP))
+    if (gb.buttons.repeat(BUTTON_UP, 5))
     {
         gb.display.drawRect(ship_pos_x, --ship_pos_y, 5, 5);
     }
 
-    if (gb.buttons.pressed(BUTTON_DOWN))
+    if (gb.buttons.repeat(BUTTON_DOWN, 5))
     {
         gb.display.drawRect(ship_pos_x, ++ship_pos_y, 5, 5);
     }
 
-    if (gb.buttons.pressed(BUTTON_RIGHT))
+    if (gb.buttons.repeat(BUTTON_RIGHT, 5))
     {
         gb.display.drawRect(++ship_pos_x, ship_pos_y, 5, 5);
     }
 
-    if (gb.buttons.pressed(BUTTON_LEFT))
+    if (gb.buttons.repeat(BUTTON_LEFT, 5))
     {
         gb.display.drawRect(--ship_pos_x, ship_pos_y, 5, 5);
+    }
+
+    if (ship_pos_y < 6)
+    {
+        ship_pos_y = 6;
+    }
+    if ((ship_pos_y + 5) >= gb.display.height())
+    {
+        ship_pos_y = 63 - 5;
+    }
+    if (ship_pos_x <= 0)
+    {
+        ship_pos_x = 0;
+    }
+    if ((ship_pos_x + 5) >= gb.display.width())
+    {
+        ship_pos_x = gb.display.width() - 5;
     }
 
     //Astéroides : Apparaissent de façon aléatoire de 0(+5 dimensions) à 69(-5) et se dirigent à une speed_x de -1
